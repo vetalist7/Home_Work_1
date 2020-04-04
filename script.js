d5fe9598b22a63d8933195c9c9a656cc68e8480c
Vitalii Bakymenko <vetalist7@ukr.net>
2020-04-04 21:17:45 +0300

    Третій коміт
    Додано цикл, в двох варіаціях. ДЗ-3

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let money = prompt("Ваш бюджет на місяць?", ''),
+'use script'
+
+let money = +prompt("Ваш бюджет на місяць?", ''),
 	time = prompt('Введіть дату в форматі YYYY-MM-DD', '');
 
 let appData = {
@@ -10,14 +12,46 @@ let appData = {
 	savings: false
 };
 
-let a1 = prompt("Введіть обов'язкову статттю трат в цьому місяці", ''),
-	a2 = prompt("В яку суму обійдеться?", ''),
-	a3 = prompt("Введіть обов'язкову статттю трат в цьому місяці", ''),
-	a4 = prompt("В яку суму обійдеться?", '');
+// for (let i = 0; i < 2; i++) {
+// 	let a = prompt("Введіть обов'язкову статттю трат в цьому місяці", ''),
+// 		b = prompt("В яку суму обійдеться?", '');
+		
+// if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+// 	&& a != '' && b != '' && a.length< 50 ) {
+// 	console.log('done');	
+// 	appData.expenses[a] = b;
+// } else {
+// 	console.log("Bad result!");
+// 	i--;
+// 	};
+// };
 
-appData.expenses[a1] = a2;
-appData.expenses[a3] = a4;
+let i = 0;
+while (i < 2) {
+	let a = prompt("Введіть обов'язкову статттю трат в цьому місяці", ''),
+		b = prompt("В яку суму обійдеться?", '');
+		i++;
 
-console.log(appData);
+if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+	&& a != '' && b != '' && a.length< 50 ) {
+	console.log('done');	
+	appData.expenses[a] = b;
+} else {
+	console.log("Bad result!");
+	i--;
+	};
+};
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Щоденний бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+	console.log("Мінімальний рівень");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log("Середній рівень");
+} else if (appData.moneyPerDay > 2000) {
+	console.log("Високий рівень");
+} else {
+	console.log("Помилка!");
+};
